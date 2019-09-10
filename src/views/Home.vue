ddea02ee15c41c45b6063db99a14e8326c30446a
Dave Hulme <David.Hulme@mitie.com>
2019-09-10 15:57:23 +0100

    Vuetify and first demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <audio :src="publicPath + 'song.mp3'" type="audio/mpeg" ref="audio" />
+    <button @click="playPause">Play/pause</button>
+    <input type="range" min="0" max="2" step="0.01" v-model="gain" />
+    <div ref="test" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import peaks from "peaks.js";
+
+const audioContext = new AudioContext();
+const gainNode = audioContext.createGain();
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      publicPath: process.env.BASE_URL,
+      gain: 1
+    };
+  },
+  mounted() {
+    console.log(this.$refs.audio);
+    const track = audioContext.createMediaElementSource(this.$refs.audio);
+    track.connect(gainNode).connect(audioContext.destination);
+    peaks.init({
+      containers: {
+        overview: this.$refs.test
+      },
+      mediaElement: this.$refs.audio,
+      audioContext,
+      height: 100
+    });
+  },
+  methods: {
+    playPause() {
+      if (audioContext.state === "suspended") {
+        audioContext.resume();
+      }
+      this.$refs.audio.play();
+    }
+  },
+  watch: {
+    gain(value) {
+      gainNode.gain.value = value;
+    }
   }
 };
 </script>
